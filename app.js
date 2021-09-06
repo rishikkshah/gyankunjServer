839bfc38b64f38b6e757ef13098cc0b7a87baca6
rishikkshah <rishikkshah987@gmail.com>
2021-09-06 12:24:12 +0545

    cors

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
