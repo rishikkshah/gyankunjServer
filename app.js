6bc1eaab20b84e5d8fafbda24e4a99907b233c9e
Rishi <65855802+rishikkshah@users.noreply.github.com>
2021-09-06 12:35:08 +0545

    Update app.js

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Access-Control-Allow-Headers", "*"
   );
   next();
 });
